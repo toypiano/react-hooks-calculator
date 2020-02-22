4b05c703287beb01bb4c42a05970d6c8d640fc93
Hayoun Lee <sidhlee@gmail.com>
2020-02-22 10:53:59 +0900

    Fix evaluate to disconcern tailing operator

--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,20 @@
-// const MAX_DIGIT = 21;
+// TODO: add input for precision setting
+const PRECISION = 2;
 
-const isOperator = val => /[/*\-+]/.test(val);
-const isNumber = val => /[0-9]/.test(val);
-const isEval = val => /Enter|=/i.test(val);
-const isClear = val => /clear/i.test(val);
-const isDecimal = val => /\./.test(val);
+const isOperator = val => /^[/*\-+]$/.test(val.trim());
+const isNumber = val => /^[0-9]$/.test(val);
+const isEval = val => /^Enter$|^=$/i.test(val);
+const isClear = val => /^clear$/i.test(val);
+const isDecimal = val => /^\.$/.test(val);
+const endsWithOperator = val => isOperator(val.trim().slice(-1));
 
 export const initialState = {
   inputVal: null,
   currentVal: "0",
+  evaluated: false,
   prevVal: "0",
   formula: "",
-  currentSign: "pos",
-  lastClicked: "",
-  digitMaxed: false
+  error: false
 };
 
 function initialized() {
@@ -21,9 +22,15 @@ function initialized() {
 }
 
 function numberEntered(state, inputVal) {
-  const { currentVal, formula } = state;
-
+  const { currentVal, formula, evaluated } = state;
   let newCurrentVal, newFormula;
+
+  if (evaluated && !endsWithOperator(formula))
+    return {
+      ...initialState,
+      currentVal: inputVal,
+      formula: inputVal
+    };
   if (currentVal === "0") {
     if (inputVal === "0") newCurrentVal = "0";
     else newCurrentVal = inputVal;
@@ -59,10 +66,16 @@ function decimalEntered(state, inputVal) {
 }
 
 function operatorEntered(state, inputVal) {
-  const { currentVal, formula } = state;
+  const { currentVal, formula, evaluated, prevVal } = state;
   let newCurrentVal = currentVal,
     newFormula = formula;
-  if (inputVal === "-" && !isOperator(currentVal.trim().slice(-1))) {
+  if (evaluated)
+    return {
+      ...state,
+      currentVal: inputVal,
+      formula: prevVal + " " + inputVal + " "
+    };
+  if (inputVal === "-" && !endsWithOperator(currentVal)) {
     if (currentVal === "-" || currentVal[0] === "-") {
       newCurrentVal = currentVal;
       newFormula = formula;
@@ -94,27 +107,34 @@ function operatorEntered(state, inputVal) {
 
 // TODO
 function evaluated(state, inputVal) {
-  return state;
+  const { currentVal, formula } = state;
+  let expression = formula;
+  if (isOperator(currentVal.trim().slice(-1)))
+    expression = formula.slice(0, -3);
+  // eslint-disable-next-line no-eval
+  const answer =
+    // eslint-disable-next-line no-eval
+    Math.round(eval(expression) * 10 ** PRECISION) / 10 ** PRECISION;
+  const newFormula = expression + " = " + answer;
+  return {
+    ...state,
+    currentVal: answer.toString(),
+    formula: newFormula,
+    prevVal: answer,
+    evaluated: true
+  };
 }
 
 function inputReceived(state, action) {
   const v = action.payload;
-  console.log("inputReceived: " + v);
-  if (isNumber(v)) {
-    return numberEntered(state, v);
+  if (state.error) {
+    return isClear(v) ? initialized(state, v) : state;
   }
-  if (isDecimal(v)) {
-    return decimalEntered(state, v);
-  }
-  if (isClear(v)) {
-    return initialized(state, v);
-  }
-  if (isOperator(v)) {
-    return operatorEntered(state, v);
-  }
-  if (isEval(v)) {
-    return evaluated(state, v);
-  }
+  if (isNumber(v)) return numberEntered(state, v);
+  if (isDecimal(v)) return decimalEntered(state, v);
+  if (isClear(v)) return initialized(state, v);
+  if (isOperator(v)) return operatorEntered(state, v);
+  if (isEval(v)) return evaluated(state, v);
   return state;
 }
 
