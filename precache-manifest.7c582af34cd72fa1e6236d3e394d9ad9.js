01cd6ffddbeb60bbceb112133f9f2ea1c9e76484
Sid Hayoun Lee <sidhlee@gmail.com>
2021-04-14 23:48:52 -0400

    Updates

---
+++ precache-manifest.7c582af34cd72fa1e6236d3e394d9ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0a6148399c2cc507b6b6b8a34198c4",
+    "url": "/crunch/index.html"
+  },
+  {
+    "revision": "ffca98c0a9a366a00b56",
+    "url": "/crunch/static/css/main.ffec74ec.chunk.css"
+  },
+  {
+    "revision": "e3cc00cfef936e115afd",
+    "url": "/crunch/static/js/2.a29c20fb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/crunch/static/js/2.a29c20fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffca98c0a9a366a00b56",
+    "url": "/crunch/static/js/main.5f8005a1.chunk.js"
+  },
+  {
+    "revision": "6fa6ebc14ee0c438b979",
+    "url": "/crunch/static/js/runtime-main.750cbe83.js"
+  }
+]);
